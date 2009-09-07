7c20768821359c91036d6331a18e162efa706adf
reiern70 <reiern70@6754ba9e-97c3-11de-bff5-93fd3790126f>
2009-09-07 11:44:58 +0000

    comments added

---
+++ struts2-jq-antilia-test/src/main/java/com/antilia/struts2/JQueryGridDBJSONAction.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.antilia.struts2;
+
+import com.antilia.struts2.jquery.model.GridModel.TransferProtocol;
+
+/**
+ * @author Ernesto Reinaldo Barreiro (reirn70@gmail.com)
+ *
+ */
+public class JQueryGridDBJSONAction extends JQueryGridDBAction {
+
+
+	private static final long serialVersionUID = 1L;
+
+	/* (non-Javadoc)
+	 * @see com.antilia.struts2.JQueryGridDBAction#getTransferProtocol()
+	 */
+	@Override
+	protected TransferProtocol getTransferProtocol() {
+		return TransferProtocol.json;
+	}
+
+}
