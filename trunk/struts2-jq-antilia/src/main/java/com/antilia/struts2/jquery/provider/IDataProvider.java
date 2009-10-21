efcb4d9a41f350176441d1671e355b136fed7a4f
reiern70 <reiern70@6754ba9e-97c3-11de-bff5-93fd3790126f>
2009-10-21 11:43:03 +0000

    search screen

--- trunk/struts2-jq-antilia/src/main/java/com/antilia/struts2/jquery/provider/IDataProvider.java
+++ trunk/struts2-jq-antilia/src/main/java/com/antilia/struts2/jquery/provider/IDataProvider.java
@@ -16,17 +16,24 @@ import com.antilia.struts2.jquery.model.SortInfo;
 public interface IDataProvider<B extends Serializable> extends Serializable {
 		
 	/**
-	 * Returns an Iterable over the records starting at first and ending at firt+count.
+	 * Returns an Iterable over the records starting at first and ending at first+size.
 	 * 
-	 * @param searchBean
-	 * @param sortInfo
+	 * @param start first record to return
+	 * @param size number of records to return
+	 * @param searchBean Bean to be used for filtering the results.
+	 * @param sortInfo  which column should be used for ordering data.
+	 * @param searchFields Which fields of the seacrBean should be used to filter data.
 	 * @return
 	 */
 	Iterable<? extends B> getData(int start, int size,  B searchBean, SortInfo sortInfo, String... searchFields);
 	
 	/**
+	 * Returns the number of records.
 	 * 
-	 * @return returns the number of records.
+	 * @param searchBean Bean to be used for filtering the results
+	 * @param sortInfo which column should be used for ordering data.
+	 * @param searchFields Which fields of the seacrBean should be used to filter data.
+	 * @return Returns the number of records.
 	 */
 	int getSize(B searchBean, SortInfo sortInfo, String... searchFields); 
 	
